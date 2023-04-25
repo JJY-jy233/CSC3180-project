384b26b2694c0ed44173b5fa7468a7931ef8e286
polarisumi <131460677+polarisumi@users.noreply.github.com>
2023-04-25 17:05:02 +0800

    Add files via upload

---
+++ NodeClass.py
@@ -0,0 +1,101 @@
+# import os
+# 目前手牌大小
+def get_card_value():
+    
+    
+    return
+
+class Node:
+    def __init__(self) -> None:
+        self.node_util = 0.0
+        pass
+class DecisionNode(Node):
+    def __init__(self) -> None:
+        super.__init__()
+        self.check_p = 0.166
+        self.check = None
+        
+        self.call_p = 0.166
+        self.call = None
+        
+        self.fold_p = 0.166
+        
+        self.rs_1_3_p = 0.166
+        self.rs_3_5_p = 0.166
+        self.rs_5_p = 0.166
+        
+        self.rs_1_3 = None
+        self.rs_3_5 = None
+        self.rs_5 = None
+        
+        self.end = False
+        pass
+    
+
+# class RsNode(Node):
+#     def __init__(self) -> None:
+#         super.__init__()
+        
+#         self.one_three_p = 0.333
+#         self.one_three = None
+        
+#         self.three_five_p = 0.333
+#         self.three_five = None
+        
+#         self.over_five_p = 0.333
+#         self.over_five = None
+#         pass
+    
+# class BetNode(Node):
+#     def __init__(self) -> None:
+#         super.__init__()
+        
+#         self.call_p = 0.5*0.75
+#         self.call = None
+        
+#         self.rs_p = 0.5
+#         self.rs = None
+    
+
+
+class EvalNode:
+    
+    
+    def __init__(self,card_value:int) -> None:
+        self.hand = card_value
+        self.big = DecisionNode()
+        # self.big.call = DecisionNode()
+        # self.mid = DecisionNode()
+        self.small = DecisionNode()
+        this = self.big
+        # for i in range(4):
+        this.call = EvalNode()
+        self.build_node(3)
+        this.check = EvalNode()
+        this.rs_1_3 = EvalNode()
+        this.rs_3_5 = EvalNode()
+        this.rs_5 = EvalNode()
+        
+            
+        for i in range(5):
+            this.check = EvalNode()
+            this = this.check
+        pass
+    
+    def build_node(self,count:int) ->None:
+        for i in range(count):
+            pass
+        pass
+    
+    
+    
+class RootNode:
+    def __init__(self) -> None:
+        for i in range(169):
+            exec('self.n{} = EvalNode({}) '.format(i,i))
+        
+        
+    
+# root = RootNode()
+
+    
--- poker.py
+++ poker.py
@@ -1,5 +1,7 @@
 import random
 from itertools import combinations
+import NodeClass
+import time
 
 def display_card(card_id:int) -> str:
     
@@ -107,7 +109,7 @@ def get_max_score(seven_cards:list) -> tuple:
     return max_type, max_value
 
 class Player:
-    def __init__(self,name:str,position:int,money:int = 200,label = None) -> None:
+    def __init__(self,name:str,position:int,money:int = 10000,label = None) -> None:
         self.name = name
         self.money = money
         self.label = label
@@ -115,6 +117,12 @@ class Player:
         self.tree = None
         self.hand = []
         self.score = None
+        self.current_bet = 0
+        # self.is_fold = False
+        # # self.is_bet = False
+        # self.is_check = False
+        # self.allow_check = True
+        # self.state:state = None
         pass
     
     def change_position(self) -> None:
@@ -125,41 +133,146 @@ class Player:
         # Update decision tree
         pass
     
-    def bet(self,money:int) -> int:
-        # 
-        return 0
+    def small_blind(self,sb):
+        self.current_bet = sb
+        self.money -= sb
+        
+    def big_blind(self,bb):
+        self.current_bet = bb
+        self.money -= bb
     
-    def check(self) -> None:
-        return
+    # 没人下注时 第一个下注的人
+    def bet(self,money:int) -> int:
+        # rand
+        bet_money = random.randint(1,30)
+        if self.money > bet_money:
+            self.money -= bet_money
+            self.current_bet = bet_money
+            # self.is_bet = True
+            return bet_money
+        else:
+            allin = self.money
+            self.current_bet = allin
+            self.money = 0
+            return -3 # allin
+    
+    # def call(self,money:int) -> int:
+    #     return money
+    
+    def Raise(self,max_bet:int) -> int:
+        dif = 2*max_bet - self.current_bet
+        if self.money > dif:
+            self.money -= dif
+            self.current_bet += dif
+            return dif
+        else:
+            allin = self.money
+            self.current_bet += allin
+            self.money = 0
+            return -3
+    
+    def check(self) -> int:
+
+        return -1
+    
+    def call(self,money:int) -> int:
+        # 补差码
+        dif = money - self.current_bet
+        if self.money > dif:
+            self.money -= dif
+            self.current_bet += dif
+            return dif
+        else:
+            allin = self.money
+            self.current_bet += allin
+            self.money = 0
+            return -3
     
 
-    def fold(self) -> None:
-        self.hand.clear()
-        return
+    def fold(self) -> float:
+        self.hand = []
+
+        return -2
+    
+    def new_game(self)->None:
+        pass
+        # self.tree 
+    
+    def action(self,max_bet) -> float:
+        
+        # action_label = -1
+        # search tree
+        
+        # action_label = self.tree
+        # random.choice(["check","call",'asd'])
+        
+        
+        # # 没人下注的话，可以check和bet（一般不直接fold）
+        # if(max_bet==0):
+        #     action_label = random.choice([1,2])
+            
+        # # 有人下注就不能check，只能fold，call，raise
+        # else:
+        #     action_label = random.choice([0,3,4])
+        action_label = int(input())
+        
+
+            
+        if action_label == 0:
+            money = self.fold()
+        elif action_label == 1:
+            money = self.check()
+        elif action_label == 2:
+            money = self.bet(2)
+        elif action_label == 3:  # call
+            money = self.call(max_bet)
+        else:
+            money = self.Raise(max_bet)
+        return money
+        
     
     def get_max_score(self) -> None:
         
         return
+
+# class state:
+#     def __init__(self):
+#         self.allow_check = True
+#         pass
     
 
 class Game:
 
-    def __init__(self,players_list:list[Player] = []) -> None:  
+    def __init__(self,players_list:list[Player] = [],bigblind:int = 2,smallblind:int = 1) -> None:  
         
         # [0~51] 0:2♠ 1:2♥ .... 50: A♦ 51:A♣
         self.cards = [i for i in range(52)]
         random.shuffle(self.cards)
         self.public_cards = []
         self.players = players_list
-        self.rest_players = players_list
+        self.rest_players = self.players.copy()
         self.players_num = len(players_list)
         self.card_position = 0
         self.pot = 0
         self.round = 0
+        self.bigblind = bigblind
+        self.smallblind = smallblind
+        self.allin = []
         pass
     
-# not finished
+    def is_terminal(self):
+        if len(self.rest_players) == 1:
+            return True
+        if self.round == 5:
+            return True
+        return False
+    
+
     def show_hand(self) -> None:
+        # allin的人
+        for i in self.allin:
+            self.rest_players.append(self.allin.pop())
+        
         biggest_type = -1
         for player in self.rest_players:
             seven_cards = self.public_cards + player.hand
@@ -169,14 +282,14 @@ class Game:
                 biggest_type = scores[0]
         
         # 最大牌型的玩家们
-        winners = []
+        winners:list[Player] = []
         for player in self.rest_players:
             if player.score[0] == biggest_type:
                 winners.append(player)
         
         # 如果只有一个玩家具有最大牌型，独赢
         if len(winners) == 1:
-            print(winners[0].name)
+            # print(winners[0].name)
             self.end_game(winners[0])
             return
 
@@ -201,7 +314,7 @@ class Game:
                     
                 # 如果只剩最后一个winner    
                 if len(winners) == 1:
-                    print(winners[0].name)
+                    # print(winners[0].name)
                     self.end_game(winners[0])
                     return
                 
@@ -220,44 +333,60 @@ class Game:
         part_pot = self.pot / len(winners)
         for winner in winners:
             winner.money += part_pot
-            print(winner.name)
+            print(winner.name,winner.score)
             
             
-                
-                
-            
+      
                     
     
     def new_game(self) -> None:
+        # 换位置
+        if self.round != 0:
+            self.players.append(self.players.pop(0))
+            
+        # 洗牌
         random.shuffle(self.cards)
+        
+        # 清空手牌
+        for i in self.players:
+            i.hand = []
+            
+            
+        
         self.rest_players = self.players.copy()
         self.card_position = 0
         self.pot = 0
+        self.public_cards = []
         # change position 
-        if self.round != 0:
-            self.players.append(self.players.pop(0))
         self.round = 0
         
+    
+    # 玩家进场 
     def add_player(self,new:Player) -> None:
         self.players.insert(new.position,new)
         self.players_num += 1
         
+    # 玩家离场，num：index of player
     def delete_player(self,num:int) -> Player:
         if num > self.players_num:
             raise 'This play does not exist' 
         self.players_num -= 1
         return self.players.pop(num)
         
-    
+    # 发手牌
     def deal_card(self) -> None: 
         for i in range(2):
             for j in range(self.players_num):
-                self.players[j].hand.append(self.cards[self.card_position])
+                self.rest_players[j].hand.append(self.cards[self.card_position])
+                if (len(self.rest_players[j].hand)) > 2:
+                    print(self.rest_players[j].hand)
                 self.card_position += 1 
         return
  
+    # num：发几张公牌
     def deal_public_cards(self,num:int) -> None:
         if num == 3:
+            # 切一张牌
             self.card_position += 1
             for i in range(3):
                 self.public_cards.append(self.cards[self.card_position])
@@ -269,15 +398,215 @@ class Game:
         return
     
     def action(self) -> None:
-        for i in self.players:
-            self.pot += i.bet()
+        for i in self.rest_players:
+            i.current_bet = 0
+        i = 0
+        # smallblinder = self.rest_players[0]
+        last_p = self.rest_players[-1]
+        check_last_p = self.rest_players[-1]
+        current_bet = 0
+        while len(self.rest_players) !=0:
+            
+            p = self.rest_players[i]
+
+            i+=1
+            
+            # 玩家的下注量 可能是负数（fold：-2，check：-1）
+            player_bet = p.action(current_bet)
+            self.pot += max(0,player_bet)
+            
+            # 当前一轮最大下注筹码
+            current_bet = max(player_bet,current_bet)
+            
+            # 弃牌，
+            if player_bet == -2:
+                self.rest_players.remove(p)
+                # print(p.name,"fold")
+                i-=1
+            
+            # allin
+            if player_bet == -3:
+                self.rest_players.remove(p)
+                self.allin.append(p)
+                i-=1
+            
+            # 到最后一个人说完话时，这一轮结束
+            if p == last_p:
+                break
+            
+            
+            # check后，此玩家跳过，放到最后一个人说话
+            if player_bet == -1:
+                # self.rest_players.append(self.rest_players.pop(0))
+                
+                # print(p.name,"check")
+                if check_last_p == p:
+                    break
+                # check_count += 1
+                last_p = p
+                # i-=1
+                
+            if i > (len(self.rest_players) - 1):
+                i = 0
+           
             # update i matirx
+        
+        # 恢复说话顺序
+        # if check_count != len(self.rest_players):
+        #     for i in range(check_count):
+        #         self.rest_players.insert(0,self.rest_players.pop())
+            
+            
+    def action_first_round(self) -> None:
+        i = 2
+        # smallblinder = self.rest_players[0]
+        last_p = self.rest_players[1]
+        self.rest_players[0].small_blind(self.smallblind)
+        self.rest_players[1].big_blind(self.bigblind)
+        check_last_p = self.rest_players[1]
+        bet_last_p = self.rest_players[1]
+        self.pot = self.bigblind + self.smallblind
+        max_bet = self.bigblind
+        while len(self.rest_players) !=0:
+            
+            p = self.rest_players[i]
+            
+            # 到最后一个人说完话时，这一轮结束
+            if p == last_p:
+                break
+            # 玩家的下注量 可能是负数（fold：-2，check：-1）
+            player_bet = p.action(max_bet)
+            self.pot += max(0,player_bet)
+            
+            # allin
+            if player_bet == -3:
+                self.rest_players.remove(p)
+                self.allin.append(p)
+                i-=1
+            # 弃牌，
+            elif player_bet == -2:
+                self.rest_players.remove(p)
+                # print(p.name,"fold")
+                i-=1
+            
+            # 到最后一个人说完话时，这一轮结束
+            # if p == last_p:
+            #     break
+            
+            # 当前一轮最大下注筹码
+            if player_bet > max_bet:
+                max_bet = player_bet
+                last_p = p
+            
+            
+            # check后，此玩家跳过，放到最后一个人说话
+            if player_bet == -1:
+                # self.rest_players.append(self.rest_players.pop(0))
+                
+                # print(p.name,"check")
+                if check_last_p == p:
+                    break
+                # check_count += 1
+                # last_p = p
+                # i-=1
+                
+            if i > (len(self.rest_players) - 1):
+                i = 0
+            i+=1
+            # update i matirx
+        
+        # 恢复说话顺序
+        # if check_count != len(self.rest_players):
+        #     for i in range(check_count):
+        #         self.rest_players.insert(0,self.rest_players.pop())
             
             
     def end_game(self,player:Player) -> None:
         player.money += self.pot
         for i in self.players:
             i.learn()
+            
+    
+    def one_play(self):
+        # 洗牌，清空彩池
+        self.new_game()
+        
+        
+        # 给每个玩家发手牌
+        self.deal_card()
+        
+        # 第一轮下注
+        self.action_first_round()
+        
+        print("第一轮")
+        
+        for i in self.rest_players:
+            print(i.name)
+        
+        # 如果只剩一个玩家了，独赢
+        if len(self.rest_players) == 1:
+            self.end_game(self.rest_players[0])
+            return
+        if len(self.rest_players) == 0:
+            print("0",len(self.allin))
+            for i in self.players:
+                print(i.money)
+        
+        # 发三张公牌
+        self.deal_public_cards(3)
+        
+        # 第二轮下注
+        self.action()
+        
+        print("第2轮")
+        
+        for i in self.rest_players:
+            print(i.name)
+        
+        # 如果只剩一个玩家了，独赢
+        if len(self.rest_players) == 1:
+            self.end_game(self.rest_players[0])
+            return
+        
+        # 发转牌
+        self.deal_public_cards(1)
+        
+        # 第三轮下注
+        self.action()
+        
+        print("第3轮")
+        
+        for i in self.rest_players:
+            print(i.name)
+        
+        # 如果只剩一个玩家了，独赢
+        if len(self.rest_players) == 1:
+            self.end_game(self.rest_players[0])
+            return
+        
+        # 发河牌
+        self.deal_public_cards(1)
+        
+        # 第四轮下注
+        self.action()
+        
+        print("第4轮")
+        
+        for i in self.rest_players:
+            print(i.name)
+        
+        # 如果只剩一个玩家了，独赢
+        if len(self.rest_players) == 1:
+            self.end_game(self.rest_players[0])
+            return
+        
+        # show hand
+        self.show_hand()
+        
+
+        
+
+        
               
             
 
@@ -295,33 +624,50 @@ def oo(n1,n2):
     n2 += 2
 
 if __name__ == "__main__":
-    Jack = Player('2',1,300)
-    Bob = Player('1',2,350)
-    Amy = Player('3',3)
-    Cat = Player('4',4)
-    Dog = Player('5',5)
+    
+    # root = NodeClass.RootNode()
+    Jack = Player('1',1,100)
+    Bob = Player('2',2,100)
+    Amy = Player('3',3,100)
+    # Cat = Player('4',4,100)
+    # Dog = Player('5',5,100)
     players_list = [Jack,Bob,Amy]
     game = Game(players_list)
-    game.delete_player(0)
-    game.add_player(Jack)
-    game.add_player(Cat)
-    game.add_player(Dog)
-    game.deal_card()
-    for i in game.players:
-        print(i.name)
-        display_hand(i.hand)
+    # game.delete_player(0)
+    # game.add_player(Jack)
+    # game.add_player(Cat)
+    # game.add_player(Dog)
+    # game.deal_card()
+    # for i in game.players:
+    #     i.action()
+    # for i in game.players:
+    #     print(i.name)
+    #     display_hand(i.hand)
         
-    game.deal_public_cards(3)
-    display_hand(game.public_cards)
-    game.deal_public_cards(1)
-    display_hand(game.public_cards)
+    # game.deal_public_cards(3)
+    # display_hand(game.public_cards)
+    # game.deal_public_cards(1)
+    # display_hand(game.public_cards)
     
-    game.deal_public_cards(1)
-    display_hand(game.public_cards)
+    # game.deal_public_cards(1)
+    # display_hand(game.public_cards)
     
-    game.show_hand()
+    # game.show_hand()
+    # for i in game.players:
+    #     print(i.score)
+    s = time.time()
+    i = 0
+    for i in range(1):
+        print("game: ",i)
+        game.one_play()
+        # time.sleep(3)
+        i+=1
     for i in game.players:
-        print(i.score)
+        print(i.name,i.money)
+    e = time.time()
+    print(e-s)
+    # int("-------------------------------------------------------------------------------------------------------")
+        
         
 
     
@@ -330,5 +676,4 @@ if __name__ == "__main__":
     
 
                 
-
 
